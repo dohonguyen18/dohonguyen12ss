a15b4182f06cb46f7d4c24febf9729f574f76b75
dohonguyen18 <65523740+dohonguyen18@users.noreply.github.com>
2020-06-22 12:10:40 +0700

    Update app.js

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.engine('hbs',expressHandlebars({
 
 app.set('view engine','hbs');
 
-app.listen(5000,() => {
+app.listen(process.env.PORT || 5000,() => {
     console.log("Server is listening on Port 5000");
 })
 
