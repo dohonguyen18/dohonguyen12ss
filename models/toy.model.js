c196bc2550704bb05040f0d023faff5b7ad60708
dohonguyen18 <65523740+dohonguyen18@users.noreply.github.com>
2020-06-22 11:31:32 +0700

    up

---
+++ models/toy.model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+
+var toySchema = new mongoose.Schema({
+    name:{
+        type:String,
+        
+    },
+    price:{
+        type:String
+    },
+    amount:{
+        type:String
+    },
+    description:{
+        type:String
+    }
+})
+
+
+
+
+
+mongoose.model('Toy',toySchema);
